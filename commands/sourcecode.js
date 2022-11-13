8abe9c92f77f0dcf9fa73f33d651dc515d99a908
James Wygocki <psychomanical@gmail.com>
2022-11-13 03:11:33 -0600

    Fixed description of source code command

--- commands/sourcecode.js
+++ commands/sourcecode.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('sourcecode')
-		.setDescription('Provides link to GitHub repo for this code'),
+		.setDescription('Provides link to GitHub repo for this bot'),
 	async execute(interaction) {
 		await interaction.reply('https://github.com/Nitrothegreat/NitroBot');
 	},
